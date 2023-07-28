db12561d26df52d855f2693471c66e580e13388b
Pranav Rajveer <rajveer23pr@gmail.com>
2023-07-28 12:24:54 +0530

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -11,7 +11,7 @@ export default function DashboardPage(){
         <div className="h-full">
             <LandingNavbar/>
             <LandingHero/>
-            <LandingContent/>
+{/*             <LandingContent/> */}
         </div>
     )
 }
